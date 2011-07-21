b57c318963fe15da3518e976f5c739f69921ec88
Sergi Mansilla <sergi.mansilla@gmail.com>
2011-07-21 15:47:51 +0200

    Refactored FTP to work with streams, got rid of Gab dependency

--- jsftp.js
+++ jsftp.js
@@ -1,32 +1,34 @@
 var Net = require("net");
-var Gab = require("./support/gab/gab");
 var ftpPasv = require("./ftpPasv");
+var S = require("./streamer")
 
 var FTP_PORT = 21;
 
 var RE_PASV = /[-\d]+,[-\d]+,[-\d]+,[-\d]+,([-\d]+),([-\d]+)/;
-var RE_MULTILINE = /(\d\d\d)-/;
+var RE_MULTILINE = /^(\d\d\d)-/;
 var RE_RESPONSE = /^(\d\d\d)\s(.*)/;
+var RE_NL_END = /\r\n$/;
+var RE_NL = /\r\n/;
 
 var COMMANDS = [
     // Commands without parameters
     "ABOR", "PWD", "CDUP", "NOOP", "QUIT", "PASV", "SYST",
     // Commands with one or more parameters
     "CWD", "DELE", "LIST", "MDTM", "MKD", "MODE", "NLST", "PASS", "RETR", "RMD",
-    "RNFR", "RNTO", "SITE", "STAT", "STOR", "TYPE", "USER"
+    "RNFR", "RNTO", "SITE", "STAT", "STOR", "TYPE", "USER", "PASS"
 ];
 
 var Ftp = module.exports = function (cfg) {
-    Gab.apply(this, arguments);
-
     // Generate generic methods from parameter names. They can easily be
     // overriden if we need special behavior. They accept any parameters given,
     // it is the responsability of the user to validate the parameters.
-    var self = this;
+    this.raw = {};
+
     COMMANDS.forEach(function(cmd) {
         var lcCmd = cmd.toLowerCase();
-        if (!self[lcCmd]) {
-            self[lcCmd] = function() {
+        if (!this.raw[lcCmd]) {
+            var self = this;
+            this.raw[lcCmd] = function() {
                 var fullCmd = cmd;
                 var callback;
 
@@ -36,190 +38,152 @@ var Ftp = module.exports = function (cfg) {
                     if (typeof args[args.length - 1] == "function")
                         callback = args.pop();
 
-                    fullCmd += args.length ? " " + args.join(" ") : "";
+                    if (args.length)
+                        fullCmd += " " + args.join(" ");
                 }
 
-                self.processCmd(fullCmd, callback);
-
-                return self;
+                self.push(fullCmd, callback);
             };
         }
-    });
+    }, this);
 
     // Inizialization of connection and credentials details
-    var port = this.port = cfg.port || FTP_PORT;
-    var host = this.host = cfg.host;
-    var user = this.user = cfg.user;
-    var pass = this.pass = cfg.pass;
+    var port = cfg.port || FTP_PORT;
+    var host = cfg.host;
+    var user = cfg.user;
+    var pass = cfg.pass;
 
-    // The `data variable collects the raw incoming data coming from the server
-    this.data = "";
-    this.commands = [];
-    this.response = [];
-    this.customResponse = {};
-    // This variable is set to the current response code when we are in
-    // a multiline response. The value `0` means that we are not in a multiline
-    // response
-    this.inMultiline = 0;
-    // Standard FTP EOF terminator
-    this.setTerminator("\r\n");
-    this.setEncoding("utf8");
+    var socket = this.createSocket(port, host);
 
-    // The first time we connect to the FTP server we run the `USER` command
-    // and set the (post-connect) handler to `ftpHandleConnect`.
-    this.handler = this.ftpHandleConnect;
-    this.processCmd("USER " + this.user);
+    var cmd;
+    this.push = function(command, callback) {
+        cmd([command, callback]);
+        socket.write(command + "\r\n");
+    };
 
-    this.connect(port, host);
-};
+    // Stream of incoming data.
+    var input = function(next, stop) {
+        socket.on('data', next);
+        socket.on('end', stop);
+        socket.on("error", stop);
+    };
 
-Ftp.prototype = new Gab;
-Ftp.prototype.constructor = Ftp;
+    function requests(source) {
+        var NL = "\n";
+        var buffer = [];
+        var currentCode = 0;
 
-Ftp.handleResponse = {
-    "USER": function(res) {
-        if (res.code === "230")
-            return; // user accepted
+        return function stream(next, stop) {
+            source(function(data) {
+                var lines = data.replace(RE_NL_END, "").replace(RE_NL, NL).split(NL)
 
-        if (res.code === "331" || res.code === "332") {
-            this.push("PASS " + this.pass + "\r\n");
-            this.handler = Ftp.handleResponse.PASS;
+                lines.forEach(function(line) {
+                    var simpleRes = RE_RESPONSE.exec(line);
+                    var multiRes;
+
+                    if (simpleRes) {
+                        var code = parseInt(simpleRes[1]);
+                        if (buffer.length) {
+                            buffer.push(line);
+                            if (currentCode === code) {
+                                var _currentCode = currentCode;
+                                var _buffer = buffer.join("\n");
+
+                                buffer = [];
+                                currentCode = 0;
+
+                                next({
+                                    code: _currentCode,
+                                    text: _buffer
+                                });
+                            }
+                        }
+                        else {
+                            next({
+                                code: code,
+                                text: line
+                            });
+                        }
+                    }
+                    else if (!buffer.length && (multiRes = RE_MULTILINE.exec(line))) {
+                        currentCode = parseInt(multiRes[1]);
+                        buffer.push(line);
+                    }
+                    else {
+                        buffer.push(line);
+                    }
+
+                }, this);
+            }, stop);
         }
-        else
-            throw new Error("ftp login failed: user name not accepted");
-    },
-    "PASS": function(res) {
-        if (res.code === "230") {
-            return; // user and password accepted
-        }
-        else {
-            throw new Error("ftp login failed: password not accepted");
-        }
-    },
-    /*
-    "PASV": function(res) {
-        if (res.code !== "227")
-            return; // pasv failed
-
-        var match = RE_PASV.exec(res.line);
-        if (!match)
-            return; // bad port
-
-        var port = (parseInt(match[1]) & 255) * 256 + (parseInt(match[2]) & 255);
-        // establish data connection
-        this.dataConn = new ftpPasv(this.host, port);
     }
-    */
+
+    var cmds = function(next, stop) {
+        cmd = next;
+    };
+
+    var tasks = S.zip(requests(input), S.append(S.list(null), cmds));
+
+    tasks(this.parse.bind(this), function(){});
 };
 
 (function() {
-    this.collectIncomingData = function(data) {
-        this.data += data;
+
+    this.createSocket = function(port, host) {
+        this.socket = Net.createConnection(port, host);
+        this.socket.setEncoding("utf8");
+        return this.socket;
     };
 
-    this.foundTerminator = function() {
-        var data  = this.data;
+    /**
+     * Parse is called each time that a comand and a request are paired
+     * together. That is, each time that there is a round trip of actions
+     * between the client and the server. The `exp` param contains an array
+     * with the response from the server as a first element (text) and an array
+     * with the command executed and the callback (if any) as the second
+     * element.
+     *
+     * @param action {Array} Contains server response and client command info.
+     */
+    this.parse = function(action) {
+        var ftpResponse = action[0];
+        var command     = action[1];
 
-        this.data = "";
-        this.busy = true; // proper place??
-
-        if (this.inMultiline > 0) {
-
-            // If we are inside a multiline response, we append the data into the
-            // last response buffer, instead of creating a new one.
-            var len = this.response.length;
-            if (len)
-                this.response[len - 1] += "\n" + data;
-            else
-                this.response.push(data);
-
-            // We check to see if the current data line is signaling the end of the
-            // multiline response, in which case we set the `inMultiline` flag to 0
-            var close = RE_RESPONSE.exec(data);
-            if (close && close[1] === this.inMultiline)
-                this.inMultiline = 0;
-            else
-                // In case it is not, we return immediately without processing the
-                // response (yet)
-                return;
-        } else {
-            // In case we are not in a multiline response, we check to see if the
-            // current data line response is signaling the beginning of a multiline
-            // one, in which case we set the `inMultiline` flag to the code of the
-            // respo)se and return, to continue processing the rest of the response
-            // lines.
-            this.response.push(data);
-
-            var blob = RE_MULTILINE.exec(data);
-            if (blob) {
-                this.inMultiline = blob[1];
-                return;
-            }
+        var cmdName, callback;
+        if (command) {
+            cmdName  = command[0];
+            callback = command[1] ? command[1] : null;
         }
 
-        var ftpResponse = RE_RESPONSE.exec(data);
-        if (!ftpResponse)
-            return;
+        this._log(cmdName, ftpResponse);
 
-        var response  = this.response;
-        this.response = [];
+        if (callback)
+            callback(ftpResponse);
+    };
 
-        response.forEach(function(line) {
-            console.log("R:", line);
+    this._log = function(cmd, response) {
+        console.log("\n" + (cmd || ""), response.text);
+    };
+
+    /*
+     * Below this point all the complex/composed actions for FTP.
+     */
+    this.auth = function(user, pass, callback) {
+        var self = this;
+        this.raw.user(user, function(res) {
+            if ([230, 331, 332].indexOf(res.code) > -1) {
+                self.raw.pass(pass, function(res) {
+                    if ([230, 202].indexOf(res.code) > -1)
+                        callback(res);
+                    else if (res.code === 332)
+                        self.raw.acct("noaccount");
+                    else
+                        callback(new Error("Login not accepted"))
+                })
+            } else {
+                callback(new Error("Login not accepted"));
+            }
         });
-
-        // process response
-        if (this.handler) {
-            // call the response handler
-            handler = this.handler;
-            this.handler = null;
-
-            var line = response[response.length - 1];
-            var group = RE_RESPONSE.exec(line);
-
-            var responseObj = {
-                line: line,
-                code: group ? group[1] : null,
-                text: group ? group[2] : null
-            };
-            handler.call(this, responseObj);
-
-            // The previous call could have set the handler, in whih case we return
-            // to follow-up command in progress.
-            if (this.handler)
-                return;
-        }
-
-        this.processCmd();
-    };
-
-    this.processCmd = function(cmd, callback) {
-        if (arguments.length) {
-            this.commands.push([cmd, callback]);
-            if (!this.busy && this.acceptingConnections)
-                this.processCmd();
-        }
-        else {
-            if (this.commands.length) {
-                var command = this.commands.shift();
-                var cmdText = command[0];
-                // Retrieve the command name
-                this.lastCmd = /^(\w+)\s*/.exec(cmdText)[1];
-
-                if (command.length === 2 && typeof command[1] === "function")
-                    this.handler = command[1];
-                else if (Ftp.handleResponse[this.lastCmd])
-                    this.handler = Ftp.handleResponse[this.lastCmd];
-                else
-                    this.handler = null;
-
-                console.log("\nC:", "'" + cmdText + "'");
-
-                this.push(cmdText + "\r\n");
-            }
-            else
-                this.busy = false;
-        }
     };
 
     this.setPassive = function(mode, callback) {
@@ -236,9 +200,11 @@ Ftp.handleResponse = {
         });
     };
 
-    // Downloads a file from FTP server, given a valid Path. It uses the RETR
-    // command to retrieve the file. the `get` and `retr` methods are synonymous of
-    // this method.
+    /**
+     * Downloads a file from FTP server, given a valid Path. It uses the RETR
+     * command to retrieve the file. the `get` and `retr` methods are synonymous of
+     * this method.
+     */
     this.retrBinary = function(filePath, callback) {
         var mode = "I";
         this.type(mode, function(res) {
@@ -259,13 +225,5 @@ Ftp.handleResponse = {
         });
     };
 
-    // Connect handler
-    this.ftpHandleConnect = function(res) {
-        if (res.code === "220")
-            this.acceptingConnections = true;
-        else
-            throw new Error("ftp login failed");
-    };
-
-}).call(Ftp.prototype);
+}).call(Ftp.prototype)
 
